7ba263adc0ee8d072d103c39232d2fdf70785511
Ezechiel KIFALANG <kifaeric2002@gmail.com>
2026-03-03 13:25:59 +0100

    update

--- si_barrage/main.py
+++ si_barrage/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 
+
 from .db import get_db
 from .modules.meteo import router as meteo_router
 from .modules.maintenance import router as maintenance_router
@@ -18,9 +19,15 @@ app.include_router(meteo_router.router, prefix="/meteo", tags=["Météo"])
 app.include_router(maintenance_router.router, prefix="/maintenance", tags=["Maintenance"])
 app.include_router(production_router.router, prefix="/production", tags=["Production"])
 
+from .modules.maintenance import ui_router as maintenance_pages
+app.include_router(maintenance_pages.router, prefix="/home", tags=["UI Maintenance"])
+
+
 @app.get("/", tags=["Root"])
 def read_root():
     return {"message": "Bienvenue sur l'API du SI Barrage"}
+
+
 
 
 @app.get("/db", tags=["Database"])
